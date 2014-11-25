e609d644ee358cd5ed06be59bad4114ac1154fc8
Ibnu Yahya <anak10thn@gmail.com>
2014-11-25 13:48:24 +0700

    Yeah

---
+++ server.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
+	"net/http"
+	)
+
+func init(){
+	//martini.Env = martini.Prod
+	martini.Env = martini.Dev
+}
+
+func main() {
+  m := martini.Classic()
+  m.Use(render.Renderer(render.Options{
+	  Layout: "home",
+	}))
+
+  m.Get("/", func(r render.Render){
+    r.HTML(200, "halo", "ibnu")
+  })
+
+  m.Get("/api", Auth, func(r render.Render) {
+    r.JSON(200, map[string]interface{}{"hello": "world","id":1})
+  })
+
+  m.Run()
+}
+
+func Auth(res http.ResponseWriter, req *http.Request){
+	if req.Header.Get("API-KEY") != "secret123" {
+		http.Error(res, "Fuck Yeah", 401)
+	}
+}
